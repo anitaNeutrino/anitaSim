0312d180b19efbfe13b814fb75be06a5f8ab4bbb
qabarr <qabarr@gmail.com>
2021-03-22 14:10:41 -0400

    Add runtime counter

--- test/simulateAnita.cc
+++ test/simulateAnita.cc
@@ -3,6 +3,8 @@
 #include "EventGenerator.h"
 #include "AnitaPayload.h"
 
+#include <chrono>
+
 int main(int argc,  char **argv) {
 
   //--------------------------------------------------------------
@@ -11,6 +13,7 @@ int main(int argc,  char **argv) {
   // 12/01/03
   //
   //--------------------------------------------------------------
+  auto starttime = std::chrono::system_clock::now();
   anitaSim::Settings settings;
   icemc::CommandLineOptions clOpts(argc, argv, settings);
 
@@ -19,6 +22,10 @@ int main(int argc,  char **argv) {
     icemc::EventGenerator uhen(&settings);
     uhen.generate(*anita.get());
   }
-  
+
+  auto endtime = std::chrono::system_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(endtime - starttime);
+  int elapsed_seconds = elapsed.count();
+  std::cout << "Elapsed runtime is " << (int)(elapsed_seconds/60) << ":" << (elapsed_seconds%60) << " minutes" <<  std::endl;
   return 0;
 }
